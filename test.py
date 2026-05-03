668ae6718dfd7e0b26f3e3f7212d332efab9b559
yubraj525 <yubrajkhatri525@gmail.com>
2026-05-03 15:42:17 +0545

    fix speaker

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import os
 from groq import Groq
 from dotenv import load_dotenv
-
+from playsound import playsound
 # -----------------------------
 # INIT
 # -----------------------------
@@ -23,10 +23,42 @@ async def generate_speech(text):
     )
     response.write_to_file(speech_file_path)
     print("reponse saved")
-    from websocket import stream_audio
-    await stream_audio()
+    return speech_file_path
+
+    # to stream to esp 
+    # from websocket import stream_audio
+    # await stream_audio()
     
+
+
+
+# # if __name__ == "__main__":
+# #     import asyncio
+# #     test_text = "Hello, this is a test of the speech generation system!"
+# #     asyncio.run(generate_speech(test_text))
+
+
+
+# import winsound
+# import os
+# import time
+# from speech import play_audio
+# import asyncio
+# FILE = "response.wav"
+
+# def wait_for_file(path):
+#     print("Waiting for file...")
+
+#     while not os.path.exists(path) or os.path.getsize(path) < 1000:
+#         time.sleep(0.1)
+
+#     print("File ready!")
+
+# # def play_audio(path):
+# #     print("Playing audio...")
+# #     winsound.PlaySound(path, winsound.SND_FILENAME)
+
 # if __name__ == "__main__":
-#     import asyncio
-#     test_text = "Hello, this is a test of the speech generation system!"
-#     asyncio.run(generate_speech(test_text))
+#     wait_for_file(FILE)
+#     asyncio.run(play_audio(FILE))
+#     # play_audio(FILE)
